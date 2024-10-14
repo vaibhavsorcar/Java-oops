e2f6b04313e6ad69d2079af59e71dcc879de5880
𝚅𝚊𝚒𝚋𝚑𝚊𝚟 𝚂𝚘𝚛𝚌𝚊𝚛 <120783798+vaibhavsorcar@users.noreply.github.com>
2024-10-14 12:33:36 +0530

    Create Enums.java
    Demonstration of the concept of Enums.

---
+++ Enums.java
@@ -0,0 +1,27 @@
+enum Level {
+  LOW,
+  MEDIUM,
+  HIGH
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Level myVar = Level.MEDIUM;
+
+    switch(myVar) {
+      case LOW:
+        System.out.println("Low level");
+        break;
+      case MEDIUM:
+         System.out.println("Medium level");
+        break;
+      case HIGH:
+        System.out.println("High level");
+        break;
+    }
+
+    for (Level myVar : Level.values()) {
+  System.out.println(myVar);
+}
+  }
+}
